fac16dcc5c293b23726945b38c7dfa496e43da74
nicoSix <nicolasix@outlook.fr>
2021-04-11 02:26:21 +0200

    Adding colorful logo to login page + removing the small one from navbar if we are in login

--- app/src/components/Logo.js
+++ app/src/components/Logo.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Link as RouterLink } from 'react-router-dom';
+import { Link as RouterLink, useLocation } from 'react-router-dom';
 import { Box } from '@material-ui/core';
 
 const Logo = () => {
+  const location = useLocation();
   return (
-    <Box>
+    <Box display={location.pathname === '/login' ? 'none' : ''}>
       <RouterLink to="/app/dashboard">
         <img style={{ height: '40px' }} src="/static/logo.png" alt="Logo" />
       </RouterLink>
