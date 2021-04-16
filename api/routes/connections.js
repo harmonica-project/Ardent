076c7310b01cdef08a18aeef30184030b5fe9680
nicoSix <nicolasix@outlook.fr>
2021-04-16 02:56:34 +0200

    Bugfix: wrong connection routes

--- api/routes/connections.js
+++ api/routes/connections.js
@@ -3,13 +3,13 @@ const db = require('../data/connections');
 const { authorizedOnly } = require('../utils/authorization');
 
 router
-  .post('/connection', authorizedOnly, (req, res) => {
+  .post('/', authorizedOnly, (req, res) => {
     db.storeConnection(req.body).then((parsedResult) => {
         if(parsedResult.success) res.status(200).send(parsedResult);
         else res.status(500).send(parsedResult);
     })
   })
-  .delete('/connection/:id', authorizedOnly, (req, res) => {
+  .delete('/:id', authorizedOnly, (req, res) => {
     db.deleteConnection(req.params.id).then((parsedResult) => {
         if(parsedResult.success) res.status(200).send(parsedResult);
         else res.status(500).send(parsedResult);
