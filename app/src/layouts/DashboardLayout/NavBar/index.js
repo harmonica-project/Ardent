508323e7dc2f750c6f1356b1d63e229de5527642
nicoSix <nicolasix@outlook.fr>
2021-04-19 22:03:47 +0200

    Removing fields in paper results table (still accessible with options) and fixing doi unshrinkable cell + fixing wrong URL when clicking on the avatar

--- app/src/layouts/DashboardLayout/NavBar/index.js
+++ app/src/layouts/DashboardLayout/NavBar/index.js
@@ -129,7 +129,7 @@ const NavBar = ({ onMobileClose, openMobile }) => {
             className={classes.avatar}
             component={RouterLink}
             src={user.avatar}
-            to="/app/account"
+            to="/app/settings"
           />
         </Box>
         <Typography
