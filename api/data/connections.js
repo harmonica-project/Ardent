c5a62b8cfc91491165afa084f6cfddc1584900f7
nicoSix <nicolasix@outlook.fr>
2021-05-16 13:19:43 +0200

    Removing the limit of one connection between two components

--- api/data/connections.js
+++ api/data/connections.js
@@ -24,22 +24,13 @@ module.exports = {
     storeConnection: async connection => {
         const newConnectionId = uuidv4();
         try {
-            const foundConnections = await client.query("SELECT * FROM connections WHERE (first_component = $1 AND second_component = $2) OR (first_component = $2 AND second_component = $1)", [connection.first_component, connection.second_component]);
-            if(foundConnections["rows"].length === 0) {
-                await client.query(`
-                    INSERT INTO connections (id, first_component, second_component, datatype, direction, name) 
-                    VALUES ($1, $2, $3, $4, $5, $6)
-                `, [newConnectionId, connection.first_component, connection.second_component, connection.datatype, connection.direction, connection.name])
-                return {
-                    success: true,
-                    connectionId: newConnectionId
-                }
-            }
-            else {
-                return {
-                    success: false,
-                    errorMsg: 'Connection already exists for this component.'
-                };
+            await client.query(`
+                INSERT INTO connections (id, first_component, second_component, datatype, direction, name) 
+                VALUES ($1, $2, $3, $4, $5, $6)
+            `, [newConnectionId, connection.first_component, connection.second_component, connection.datatype, connection.direction, connection.name])
+            return {
+                success: true,
+                connectionId: newConnectionId
             }
         }
         catch(err) {
